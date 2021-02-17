4e2ca266986afc88397a375bf983a084f1526997
Matt Wolford <matthew.r.wolford@gmail.com>
2021-02-17 23:20:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,5 +13,5 @@ end
 
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  snacks.detect{ |x| x == "cheddar" || "gouda" || "camembert" }
+  snacks.include?(cheese_types)
 end
