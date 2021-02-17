eeb7459229f579ac3d29f06787e23498c9210339
Matt Wolford <matthew.r.wolford@gmail.com>
2021-02-17 23:29:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,9 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(snacks)
+  snacks.collect { |x| x == "cheddar" || "gouda" || "camembert" }
+
+
   cheese_types = ["cheddar", "gouda", "camembert"]
   snacks.any?("cheddar" || "gouda" || "camembert")
 end
