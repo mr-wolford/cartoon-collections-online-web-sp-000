912e5478d6e94bfc3721b8e59fdff226fc47866f
Matt Wolford <matthew.r.wolford@gmail.com>
2021-02-17 22:33:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(elements)
 end
 
 def long_planeteer_calls(calls)
-  calls.include? { |x| x.length > 4 }
+  calls.include?(calls.length > 4)
 end
 
 def find_the_cheese# code an argument here
